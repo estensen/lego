3b968818a9e76884d732eba9a5ae1b818812de31
Martin Farstad <farstad2k@gmail.com>
2018-02-27 17:40:53 +0100

    Add filter for search

--- lego/apps/companies/filters.py
+++ lego/apps/companies/filters.py
@@ -1,6 +1,7 @@
-from django_filters import BooleanFilter, FilterSet
+from django_filters.rest_framework import CharFilter, FilterSet, BooleanFilter
 
 from lego.apps.companies.models import Semester
+from .models import CompanyInterest
 
 
 class SemesterFilterSet(FilterSet):
@@ -9,3 +10,12 @@ class SemesterFilterSet(FilterSet):
     class Meta:
         model = Semester
         fields = ('company_interest', )
+
+
+class CompanyInterestFilterSet(FilterSet):
+    company_name = CharFilter(lookup_expr='icontains')
+    contact_person = CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = CompanyInterest
+        fields = ('company_name', 'contact_person')
